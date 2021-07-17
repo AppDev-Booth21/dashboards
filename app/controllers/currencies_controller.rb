6e94c12d7a0823824b98795535ce43204830727e
jweinheibooth <jweinhei@chicagobooth.edu>
2021-07-17 19:58:26 +0000

    Almost there

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -5,14 +5,12 @@ class CurrenciesController < ApplicationController
     @parsed_data = JSON.parse(@raw_data)
     @symbols_hash = @parsed_data.fetch("symbols")
 
-
     @array_of_symbols = @symbols_hash.keys
     
     render({ :template => "currency_templates/step_one.html.erb"})
   end
 
   def second_currency
-    
     @raw_data = open("https://api.exchangerate.host/symbols").read
     @parsed_data = JSON.parse(@raw_data)
     @symbols_hash = @parsed_data.fetch("symbols")
@@ -21,8 +19,24 @@ class CurrenciesController < ApplicationController
 
 
     @array_of_symbols = @symbols_hash.keys
-    
     render({ :template => "currency_templates/step_two.html.erb"})
+  end
+
+  def convert
+    #@raw_data = open("https://api.exchangerate.host/convert").read
+    @raw_data = open("https://api.exchangerate.host/convert?from=&to=").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash1 = @parsed_data.fetch("query")
+    @symbols_hash2 = @parsed_data.fetch("info")
+
+    @from_symbol = params.fetch("from_currency")
+    @to_symbol = params.fetch("to_currency")
+
+    @amount = @symbols_hash1.fetch("amount")
+    @rate = 
+
+    
+    render({ :template => "currency_templates/step_three.html.erb"})
   end
 
 end
